2ea060d8d91bea6403b1ded73ea67746e0e3f348
Maya <79027972026@yandex.ru>
2022-03-24 14:33:25 +0500

    add replace to url

--- src/App.tsx
+++ src/App.tsx
@@ -68,13 +68,15 @@ const mapStateToProps = (state: AppStateType): MapStatePropsType => ({
 
 let AppContainer = compose(connect(mapStateToProps, {initializeApp}))(App);
 let MainApp = () => {
-    return (<HashRouter basename={process.env.PUBLIC_URL}>
+    return (
+        /*<HashRouter basename={process.env.PUBLIC_URL}>*/
 
-            <Provider store={store}>
+        <BrowserRouter><Provider store={store}>
             <AppContainer/>
-        </Provider>
+        </Provider></BrowserRouter>
 
-    </HashRouter>)
+   /* </HashRouter>*/
+)
 }
 export default MainApp;
 
