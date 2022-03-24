5100e678ed5837ab64dcb68f0d4784ccba57a106
Maya <79027972026@yandex.ru>
2022-03-24 11:54:41 +0500

    "start": "set PORT=80 && react-scripts start"

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {BrowserRouter} from "react-router-dom";
 import MainApp from "./App";
 
 /*"homepage": "https://Mayariff.github.io/my-social-network",*/
 
     ReactDOM.render(
-        <BrowserRouter>
-            <MainApp />
-        </BrowserRouter>,
+
+            <MainApp />,
         document.getElementById('root')
     );
 
